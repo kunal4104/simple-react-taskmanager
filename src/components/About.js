db7348a49f78128c203b633819d3225bf2be520e
kunal-agt <kunalsingh.agt@gmail.com>
2021-05-03 00:19:12 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About = () => {
+	return (
+		<div>
+			<h5>Version 1.0.1</h5>
+			<Link to="/">Back</Link>
+		</div>
+	);
+};
+
+export default About;
